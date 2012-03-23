6f60096ef74c75fe7083b15beb04893134b89093
Chris Moore <moorecp@gmail.com>
2012-03-22 21:46:59 -0400

    Added a config file so you can make changes without modifying the code.

--- mpd_growl.rb
+++ mpd_growl.rb
@@ -1,22 +1,27 @@
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default, :app)
+require 'yaml'
 
 class MPDClient
-  attr_accessor :first_run, :growl, :mpd
-  NOTIFICATION_TYPES= { :song_change => "MPD Song Change Notification" }
+  attr_accessor :first_run, :growl, :growl_method, :mpd
+  NOTIFICATION_TYPES = { :song_change => "MPD Song Change Notification" }
+  GROWL_METHODS = { :growlnotify => "growlnotify", :ruby => 'ruby-growl'}
 
-  def initialize(mpd)
+  def initialize(mpd, growl_host, growl_method)
     self.mpd = mpd
     self.first_run = true
-    self.growl = Growl.new("localhost", "MPD Growl", NOTIFICATION_TYPES.values)
+    self.growl = Growl.new(growl_host, "MPD Growl", NOTIFICATION_TYPES.values)
+    self.growl_method = growl_method
   end
 
   def current_song_callback(song)
     self.first_run = false
     if song
-      self.growl.notify(NOTIFICATION_TYPES[:song_change], song.title, "#{song.artist}\n#{song.album}")
-      # system "growlnotify -n "MPD Growl" -a iTunes -t \"#{song.title}\" -m \"#{song.artist}\n#{song.album}\""
+      case self.growl_method
+      when GROWL_METHODS[:growlnotify] then system(%(growlnotify -n "MPD Growl" -a iTunes -t "#{song.title}" -m "#{song.artist}\n#{song.album}"))
+      when GROWL_METHODS[:ruby] then self.growl.notify(NOTIFICATION_TYPES[:song_change], song.title, "#{song.artist}\n#{song.album}")
+      end
     end
   end
 
@@ -26,8 +31,17 @@ class MPDClient
   end
 end
 
-mpd = MPD.new('localhost', 6600)
-client = MPDClient.new(mpd)
+def load_config
+	config = YAML.load_file(File.join(File.expand_path(File.dirname(__FILE__)), "config.yaml"))
+	@mpd_host     = config["config"]["mpd_host"]     || 'localhost'
+	@mpd_port     = config["config"]["mpd_port"]     || 6600
+  @growl_host   = config["config"]["growl_host"]   || 'localhost'
+  @growl_method = config["config"]["growl_method"] || MPDClient::GROWL_METHODS[:ruby]
+end
+
+load_config
+mpd = MPD.new(@mpd_host, @mpd_port)
+client = MPDClient.new(mpd, @growl_host, @growl_method)
 
 mpd.register_callback(client.method('current_song_callback'), MPD::CURRENT_SONG_CALLBACK)
 mpd.register_callback(client.method('state_callback'), MPD::STATE_CALLBACK)
